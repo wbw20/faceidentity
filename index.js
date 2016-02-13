ba2954bd6edc0dbdf6aae1b6f261227a7ce1bd64
William B. Wettersten <wwettersten@classpass.com>
2016-02-13 15:45:13 -0800

    person creation

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-FACE_LIST_ID = 1;
-API_KEY      = 'd30a56d36d634581b9ed4b39a9419969';
+FACE_LIST_ID    = 1;
+PERSON_GROUP_ID = 1;
+API_KEY         = 'd30a56d36d634581b9ed4b39a9419969';
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -14,6 +15,23 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('pages/index');
+});
+
+app.post('/person', function(req, res) {
+  request.post('https://api.projectoxford.ai/face/v1.0/persongroups/' + PERSON_GROUP_ID + '/persons', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': API_KEY
+    },
+    json: {
+      name: 'William Wettersten',
+      userData: 'https://scontent-sea1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/391584_4404273781433_1216832326_n.jpg?oh=4e7d0161491434586da2249bf5d5fcb8&oe=5770706B'
+    }
+  }, function (error, response, body) {
+    if (error) { console.log(error); }
+
+    res.send(body);
+  });
 });
 
 app.post('/detect', function(req, res) {
