4f8765bbfd659487c4471be9ed3586f9c57b88e3
William B. Wettersten <wwettersten@classpass.com>
2016-02-13 15:21:46 -0800

    face detection

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var express = require('express');
+var request = require('request');
+
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.post('/detect', function(req, res) {
+  request.post('https://api.projectoxford.ai/face/v1.0/detect?returnFaceId=true', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': 'd30a56d36d634581b9ed4b39a9419969'
+    },
+    json: {
+      url: 'https://scontent-sea1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/391584_4404273781433_1216832326_n.jpg?oh=4e7d0161491434586da2249bf5d5fcb8&oe=5770706B'
+    }
+  }, function (error, response, body) {
+    if (error) { console.log(error); }
+
+    res.send(body);
+  });
 });
 
 app.listen(app.get('port'), function() {
---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  function colorcycle() {
+    var colors = [
+        '#54c8eb', // light blue
+        '#4ea9de', // med blue
+        '#4b97d2', // dark blue
+        '#92cc8f', // light green
+        '#41bb98', // mint green
+        '#c9de83', // yellowish green
+        '#dee569', // yellowisher green
+        '#c891c0', // light purple
+        '#9464a8', // med purple
+        '#7755a1', // dark purple
+        '#f069a1', // light pink
+        '#f05884', // med pink
+        '#e7457b', // dark pink
+        '#ffd47e', // peach
+        '#f69078' // salmon
+    ];
+
+    var color = colors[parseInt(Math.random() * (colors.length - 1), 10)];
+      $('.funky--border').css('transition', 'border-color 15s').css('border-color', color);
+    }
+
+    setTimeout(function () {
+      colorcycle();
+      setInterval(colorcycle, 15000);
+    }, 1000);
+
+    $('button').on('click', function() {
+      alert('hello');
+    });
+});
