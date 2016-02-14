65b1a576713c593dcc725bef913f4c968eb53f2f
William B. Wettersten <wwettersten@classpass.com>
2016-02-14 10:42:42 -0800

    clean database

--- scripts/images.js
+++ scripts/images.js
@@ -1,8 +1,12 @@
 module.exports = [
-  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/222059_10150170830525776_6292892_n.jpg?oh=295095e6790f83224bb944aeb16388c4&oe=572A125F',
-  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xap1/t31.0-8/11289524_10152967789138823_6445282689648422984_o.jpg',
-  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/261529_10150249502321716_1796619_n.jpg?oh=f4423745bc15eda75eba7a12b488de3d&oe=57349640',
-  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xpf1/v/t1.0-9/1526504_10151817287490069_173644932_n.jpg?oh=23095d47838898752bd8f82f9ff84838&oe=5723535A',
-  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-prn2/t31.0-8/10514278_10152173049985069_5416686040216183400_o.jpg',
-  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xlt1/v/t1.0-9/11873412_1048354755184201_8323725246802485045_n.jpg?oh=b2348dec8f56ef71fef03569ac706425&oe=576450F6'
+  // 'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/222059_10150170830525776_6292892_n.jpg?oh=295095e6790f83224bb944aeb16388c4&oe=572A125F',
+  // 'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xap1/t31.0-8/11289524_10152967789138823_6445282689648422984_o.jpg',
+  // 'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/261529_10150249502321716_1796619_n.jpg?oh=f4423745bc15eda75eba7a12b488de3d&oe=57349640',
+  // 'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xpf1/v/t1.0-9/1526504_10151817287490069_173644932_n.jpg?oh=23095d47838898752bd8f82f9ff84838&oe=5723535A',
+  // 'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-prn2/t31.0-8/10514278_10152173049985069_5416686040216183400_o.jpg',
+  // 'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xlt1/v/t1.0-9/11873412_1048354755184201_8323725246802485045_n.jpg?oh=b2348dec8f56ef71fef03569ac706425&oe=576450F6',
+  'https://scontent-sea1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/11666116_10153434527202485_8258102697602571306_n.jpg?oh=f181d3dff6efd2b5aa5e93293dec2bab&oe=5725FF79',
+  'https://scontent-sea1-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/1484698_10153243867162485_4732220137640918948_n.jpg?oh=a8469fd7165d32cec556e864ce8b1c9e&oe=575E1A3E',
+  'https://scontent-sea1-1.xx.fbcdn.net/hphotos-xtf1/v/l/t1.0-9/12400444_10205775761979936_2113437369173081029_n.jpg?oh=272ce84acef9e84faf14f0031ca1a568&oe=57239EDF',
+  'https://scontent-sea1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/10167947_2988755878187_5299585111346935214_n.jpg?oh=eee00dd2ff5696e07c4cc4bf4d9b7bfb&oe=5725EF83'
 ];
--- scripts/upload.js
+++ scripts/upload.js
@@ -1,5 +1,11 @@
-var async   = require('async');
-var request = require('request');
+var async     = require('async');
+var request   = require('request');
+var Datastore = require('nedb');
+
+var db        = new Datastore({
+  filename: 'data/images.db',
+  autoload: true
+});
 
 var urls = require('./images');
 
@@ -7,18 +13,22 @@ var API_KEY      = 'd30a56d36d634581b9ed4b39a9419969';
 var FACE_LIST_ID = 1;
 
 async.each(urls, function(url, cb) {
-  request.post('https://api.projectoxford.ai/face/v1.0/facelists/1/persistedFaces', {
+  request.post('https://api.projectoxford.ai/face/v1.0/facelists/' + FACE_LIST_ID + '/persistedFaces', {
     headers: {
       'Content-Type': 'application/json',
       'Ocp-Apim-Subscription-Key': API_KEY
     },
     json: {
-      faceListId: FACE_LIST_ID
+      url: url,
+      userData: url
     }
   }, function (error, response, body) {
     if (error) { console.log(error); }
 
-    cb(response);
+    db.insert({
+      id: body.persistedFaceId,
+      url: url
+    }, cb);
   });
 }, function(err) {
   console.log(err);
