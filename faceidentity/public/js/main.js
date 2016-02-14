74bf7b956f22e2afa774568d01a7edd3b6acd2be
Larry Muhlstein <larrymuhlstein@gmail.com>
2016-02-14 11:28:33 -0800

    Frontend edits

---
+++ faceidentity/public/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  function colorcycle() {
+    var colors = [
+        '#54c8eb', // light blue
+        '#4ea9de', // med blue
+        '#4b97d2', // dark blue
+        '#92cc8f', // light green
+        '#41bb98', // mint green
+        '#c9de83', // yellowish green
+        '#dee569', // yellowisher green
+        '#c891c0', // light purple
+        '#9464a8', // med purple
+        '#7755a1', // dark purple
+        '#f069a1', // light pink
+        '#f05884', // med pink
+        '#e7457b', // dark pink
+        '#ffd47e', // peach
+        '#f69078' // salmon
+    ];
+
+    var color = colors[parseInt(Math.random() * (colors.length - 1), 10)];
+      $('.funky--border').css('transition', 'border-color 15s').css('border-color', color);
+    }
+
+    setTimeout(function () {
+      colorcycle();
+      setInterval(colorcycle, 15000);
+    }, 1000);
+
+    $('.search__button').on('click', function() {
+        var url = $('.search__field').val();
+
+        $.post('/detect', {
+            url: url
+        }, function(data) {
+            var result = data[0];
+
+            if (result) {
+              $.get('/similar/' + result.faceId, function(data2) {
+                var result = data2[0];
+
+                if (result) {
+                  $('.result__block').empty().append('<img src="' + result.url + '"/>');
+                } else {
+                  $('.result__block').empty().append('<h1>No matches<h1/>');
+                }
+              });
+            }
+        }, 'json');
+    });
+});
---
+++ faceidentity/scripts/images.js
@@ -0,0 +1,8 @@
+module.exports = [
+  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/222059_10150170830525776_6292892_n.jpg?oh=295095e6790f83224bb944aeb16388c4&oe=572A125F',
+  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xap1/t31.0-8/11289524_10152967789138823_6445282689648422984_o.jpg',
+  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/v/t1.0-9/261529_10150249502321716_1796619_n.jpg?oh=f4423745bc15eda75eba7a12b488de3d&oe=57349640',
+  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xpf1/v/t1.0-9/1526504_10151817287490069_173644932_n.jpg?oh=23095d47838898752bd8f82f9ff84838&oe=5723535A',
+  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-prn2/t31.0-8/10514278_10152173049985069_5416686040216183400_o.jpg',
+  'https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xlt1/v/t1.0-9/11873412_1048354755184201_8323725246802485045_n.jpg?oh=b2348dec8f56ef71fef03569ac706425&oe=576450F6'
+];
